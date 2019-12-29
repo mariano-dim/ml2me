891119b243802441d5fbc7993a4d88f4afe601c0
Mariano Di Maggio <mariano.dim@gmail.com>
2019-12-28 22:08:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Proyecto ml2me
+
---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+__author__ = 'Mariano Andres Di Maggio'
+
+import os
+import sys
+from builtins import print
+
+def main():
+    path = os.getcwd()
+
+    print('|-|-|-|-|-|-|-|-|-|-|-|-|-|-  Starting Program  |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|- \n')
+
+
+# Llamada a la funcion de arranque
+main()
